9be384304ef3b269cd083ceea062376c7dfb3bcd
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2022-11-18 22:44:37 -0300

    product not found

--- client/src/screens/Shop.jsx
+++ client/src/screens/Shop.jsx
@@ -111,23 +111,26 @@ const Shop = () => {
               <IoSearch className='icon-filter' />
             </div>
             <ul className='ul-products'>
-              {
-                productFilter.map((product) => (
-                  <li className='li-products'>
-                    <div>
-                      <div className='div-img-prod'>
-                        <img className='img-prod' src={`/api/product/smallimage/${product._id}`} />
+              {productFilter.length === 0 ?
+                (<h1 className='prod-not-found'>Produto não encontrado :(</h1>
+                ) : (
+                  productFilter.map((product) => (
+                    <li className='li-products'>
+                      <div>
+                        <div className='div-img-prod'>
+                          <img className='img-prod' src={`/api/product/smallimage/${product._id}`} />
+                        </div>
+                        <hr className='skyline'></hr>
+                        <h1 className='prod-desc' href={product.name}>
+                          {product.name}
+                        </h1>
+                        <h1 className='prod-desc' href={product.price}>
+                          {product.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                        </h1>
                       </div>
-                      <hr className='skyline'></hr>
-                      <h1 className='prod-desc' href={product.name}>
-                        {product.name}
-                      </h1>
-                      <h1 className='prod-desc' href={product.price}>
-                        {product.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                      </h1>
-                    </div>
-                  </li>
-                ))
+                    </li>
+                  ))
+                )
               }
             </ul>
           </div>
