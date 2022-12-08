f4e237c8ee0ec45f83b0785bd5bc9559b34b8c6c
israelmurilocadorin <israel.del96@gmail.com>
2022-12-07 22:31:39 -0300

    filter panel component

---
+++ client/src/components/FilterPanel/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../components/Container';
+
+const FilterPanel = () => {
+  return (
+    <Container>
+      <div className=''>
+        <h2 className=''>Filter</h2>
+      </div>
+    </Container>
+  );
+}
+
+export default FilterPanel;
