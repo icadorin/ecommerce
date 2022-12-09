41e050b9eaa50f6722ef2bba037272ae9c0d02f0
israelmurilocadorin <israel.del96@gmail.com>
2022-12-08 23:42:24 -0300

    search bar part1

---
+++ client/src/components/SearchBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../components/Container';
+
+const SearchBar = () => {
+  return (
+    <Container>
+      <div className=''>
+        <h2 className=''>Filter</h2>
+      </div>
+    </Container>
+  );
+}
+
+export default SearchBar;
