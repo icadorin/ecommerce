d4a90dbddce06f383de8bc406cddc05b25df6c91
israelmurilocadorin <israel.del96@gmail.com>
2023-01-26 23:09:31 -0300

    files changed to typescript

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/Auth/AuthProvider';
 
 ReactDOM.render(
